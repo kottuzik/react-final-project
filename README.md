ddc7ce58da293bf996881c6e983eb86135008d6f
Yonatan Snir <yonatan941@gmail.com>
2020-03-02 06:22:40 +0200

    Added things to do!

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-### React Final Project!
+## React Final Project!
 
 after you pull the project don't forget run the commend -npm install-
+I added the pdf of the project.
 
-Eva, I wish us luck. 
+### Todo:
+- create SearchField component under the 'components' dir.
+- create User component ... - maybe we will need to break/spread this component to more parts. Because we need to add "more data"
+- Todos, and Posts component. I think they could be almost the same.
+- Add todo component.
+- Add new posts.
+- Add new user.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Welcome to Our Project!</h1>
+  )
 }
 
 export default App;
