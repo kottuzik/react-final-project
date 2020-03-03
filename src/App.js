2c89fc31964383d4518810b4566ca7122aca90aa
Yonatan Snir <yonatan941@gmail.com>
2020-03-03 10:09:16 +0200

    try now to pull

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import SearchField from './components/searchField/SearchField';
 
 const App = () => {
-  let data = useSelector(state => state.users)
-  console.log(data)
   return (
     <SearchField />
   )
