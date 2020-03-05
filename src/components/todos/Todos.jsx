b754a3f45ce5392eb4a6145fc5d3354be2bd1cb8
Yonatan Snir <yonatan941@gmail.com>
2020-03-05 22:04:42 +0200

    fix loading problem

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -9,21 +9,26 @@ const Todos = ({ id }) => {
     const todos = useSelector(state => state.todos)
     const users = useSelector(state => state.users);
     const user = users.find(user => user.id === id);
-
+    
     let filterTodos = todos.filter(x => x.userId === id);
-    return(
-        <div className="todos">
-            {showAdd ? <AddTodo userId={id} setShowAdd={setShowAdd} /> : 
-            <div>
-                <button className="btn blue" onClick={() => setShowAdd(true)}>Add</button>
-
-                    <h1>{user.name} todos:</h1>
-                    <h1>Name:</h1>
-                {filterTodos.map(todo => <TodoCard key={todo.id} todo={todo} />)}
+    
+    // Check if the data existing
+    if (user){
+        return(
+            <div className="todos">
+                {showAdd ? <AddTodo userId={id} setShowAdd={setShowAdd} /> : 
+                <div>
+                    <button className="btn blue" onClick={() => setShowAdd(true)}>Add</button>
+    
+                        <h1>{user.name} Page</h1>
+                    {filterTodos.map(todo => <TodoCard key={todo.id} todo={todo} />)}
+                </div>
+                }
             </div>
-            }
-        </div>
-    )
+        )
+    } else {
+        return <div className="loading">Loading...</div>
+    }
 }
 
 export default Todos;
