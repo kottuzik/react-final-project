1c20743008e86fd376e3ad32a9f6d982ec37c36f
kottuzik <ewsik.s@gmaill.com>
2020-03-05 16:06:37 +0200

    small changes

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -7,15 +7,20 @@ import AddTodo from './AddTodo';
 const Todos = ({ id }) => {
     const [showAdd, setShowAdd] = useState(false);
     const todos = useSelector(state => state.todos)
+    const users = useSelector(state => state.users);
+    const user = users.find(user => user.id === id);
 
     let filterTodos = todos.filter(x => x.userId === id);
+    console.log("TODOS: " + todos);
 
     return(
         <div className="todos">
             {showAdd ? <AddTodo userId={id} setShowAdd={setShowAdd} /> : 
             <div>
                 <button className="btn blue" onClick={() => setShowAdd(true)}>Add</button>
-                    <h1>User name todos:</h1>
+
+                    {/* <h1>{user.name} todos:</h1> */}
+                    <h1>Name:</h1>
                 {filterTodos.map(todo => <TodoCard key={todo.id} todo={todo} />)}
             </div>
             }
