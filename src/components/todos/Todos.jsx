5e66fb160247f9dca0c5f2e9ac98d55d1029e3c6
Yonatan Snir <yonatan941@gmail.com>
2020-03-05 22:31:38 +0200

    name for user page

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -7,28 +7,19 @@ import AddTodo from './AddTodo';
 const Todos = ({ id }) => {
     const [showAdd, setShowAdd] = useState(false);
     const todos = useSelector(state => state.todos)
-    const users = useSelector(state => state.users);
-    const user = users.find(user => user.id === id);
     
     let filterTodos = todos.filter(x => x.userId === id);
     
-    // Check if the data existing
-    if (user){
-        return(
-            <div className="todos">
-                {showAdd ? <AddTodo userId={id} setShowAdd={setShowAdd} /> : 
-                <div>
-                    <button className="btn blue" onClick={() => setShowAdd(true)}>Add</button>
-    
-                        <h1>{user.name} Page</h1>
-                    {filterTodos.map(todo => <TodoCard key={todo.id} todo={todo} />)}
-                </div>
-                }
+    return(
+        <div className="todos">
+            {showAdd ? <AddTodo userId={id} setShowAdd={setShowAdd} /> : 
+            <div>
+                <button className="btn blue" onClick={() => setShowAdd(true)}>Add</button>
+                {filterTodos.map(todo => <TodoCard key={todo.id} todo={todo} />)}
             </div>
-        )
-    } else {
-        return <div className="loading">Loading...</div>
-    }
+            }
+        </div>
+    )
 }
 
 export default Todos;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 
 // Importing Components
 import Todos from '../components/todos/Todos';
@@ -8,10 +9,20 @@ import Posts from '../components/posts/Posts';
 const UserPage = () => {
     const { id } = useParams();
 
+    const users = useSelector(state => state.users);
+    const user = users.find(user => user.id === parseInt(id));
+
     return(
         <div className="user-page">
-            <Todos id={parseInt(id)} />
-            <Posts id={parseInt(id)} />
+            {user ?
+            <div>
+                <h1>{user.name} Page</h1>
+                <Todos id={parseInt(id)} />
+                <Posts id={parseInt(id)} />
+            </div>
+            :
+            <div className="loading">Loading...</div>
+            }
         </div>
     )
 }
